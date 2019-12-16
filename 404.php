d708cfdf5807820b91ccf35192b244667eb38235
Kovalev Andrey <andkov440@mail.ru>
2019-12-16 16:35:01 +0300

    Поиск, пагинация

--- 404.php
+++ 404.php
@@ -2,6 +2,10 @@
 require_once('functions.php');
 
 $not_found_content = include_template('error404.php');
-$layout_content = include_template('layout.php', ['content' => $not_found_content, 'categories' => $categories]);
+$layout_content = include_template('layout.php', [
+    'content' => $not_found_content,
+    'title' => 'DC Ply Mens 2016/2017 Snowboard',
+    'categories' => $categories
+]);
 
 print ($layout_content);
