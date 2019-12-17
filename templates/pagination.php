a375852eadf8ed878c941293ae8e1cb2c61bc6cd
Kovalev Andrey <andkov440@mail.ru>
2019-12-18 00:43:50 +0300

    Исправления

--- templates/pagination.php
+++ templates/pagination.php
@@ -3,39 +3,39 @@
 
 
         <?php if (isset($search)): ?>
-            <li class="pagination-item pagination-item-prev"><?php if ($CURRENT_PAGE > 1): ?><a
-                    href="/<?= $filename; ?>?search=<?= $search; ?>&page=<?= ($CURRENT_PAGE - 1); ?>"><?php endif; ?>
+            <li class="pagination-item pagination-item-prev"><?php if ($current_page > 1): ?><a
+                    href="/<?= $filename; ?>?search=<?= $search; ?>&page=<?= ($current_page - 1); ?>"><?php endif; ?>
                     Назад</a></li>
             <?php foreach ($pages as $page): ?>
-                <li class="pagination-item <?php if ($page == $CURRENT_PAGE) : ?>pagination-item-active<?php endif; ?>">
+                <li class="pagination-item <?php if ($page == $current_page) : ?>pagination-item-active<?php endif; ?>">
                     <a
                         href="/<?= $filename; ?>?search=<?= $search; ?>&page=<?= $page; ?>"><?= $page; ?></a></li>
             <?php endforeach; ?>
-            <li class="pagination-item pagination-item-next"><?php if ($CURRENT_PAGE < $pages_count): ?><a
-                    href="/<?= $filename; ?>?search=<?= $search; ?>&page=<?= ($CURRENT_PAGE + 1); ?>"><?php endif; ?>
+            <li class="pagination-item pagination-item-next"><?php if ($current_page < $pages_count): ?><a
+                    href="/<?= $filename; ?>?search=<?= $search; ?>&page=<?= ($current_page + 1); ?>"><?php endif; ?>
                     Вперед</a></li>
         <?php elseif (isset($category_id)): ?>
-            <li class="pagination-item pagination-item-prev"><?php if ($CURRENT_PAGE > 1): ?><a
-                    href="/<?= $filename; ?>?id=<?= $category_id; ?>&page=<?= ($CURRENT_PAGE - 1); ?>"><?php endif; ?>
+            <li class="pagination-item pagination-item-prev"><?php if ($current_page > 1): ?><a
+                    href="/<?= $filename; ?>?id=<?= $category_id; ?>&page=<?= ($current_page - 1); ?>"><?php endif; ?>
                     Назад</a></li>
             <?php foreach ($pages as $page): ?>
-                <li class="pagination-item <?php if ($page == $CURRENT_PAGE) : ?>pagination-item-active<?php endif; ?>">
+                <li class="pagination-item <?php if ($page == $current_page) : ?>pagination-item-active<?php endif; ?>">
                     <a
                         href="/<?= $filename; ?>?id=<?= $category_id; ?>&page=<?= $page; ?>"><?= $page; ?></a></li>
             <?php endforeach; ?>
-            <li class="pagination-item pagination-item-next"><?php if ($CURRENT_PAGE < $pages_count): ?><a
-                    href="/<?= $filename; ?>?id=<?= $category_id; ?>&page=<?= ($CURRENT_PAGE + 1); ?>"><?php endif; ?>
+            <li class="pagination-item pagination-item-next"><?php if ($current_page < $pages_count): ?><a
+                    href="/<?= $filename; ?>?id=<?= $category_id; ?>&page=<?= ($current_page + 1); ?>"><?php endif; ?>
                     Вперед</a></li>
         <?php else: ?>
-            <li class="pagination-item pagination-item-prev"><?php if ($CURRENT_PAGE > 1): ?><a
-                    href="/?page=<?= ($CURRENT_PAGE - 1); ?>"><?php endif; ?>Назад</a></li>
+            <li class="pagination-item pagination-item-prev"><?php if ($current_page > 1): ?><a
+                    href="/?page=<?= ($current_page - 1); ?>"><?php endif; ?>Назад</a></li>
             <?php foreach ($pages as $page): ?>
-                <li class="pagination-item <?php if ($page == $CURRENT_PAGE) : ?>pagination-item-active<?php endif; ?>">
+                <li class="pagination-item <?php if ($page == $current_page) : ?>pagination-item-active<?php endif; ?>">
                     <a
                         href="/?page=<?= $page; ?>"><?= $page; ?></a></li>
             <?php endforeach; ?>
-            <li class="pagination-item pagination-item-next"><?php if ($CURRENT_PAGE < $pages_count): ?><a
-                    href="/?page=<?= ($CURRENT_PAGE + 1); ?>"><?php endif; ?>Вперед</a></li>
+            <li class="pagination-item pagination-item-next"><?php if ($current_page < $pages_count): ?><a
+                    href="/?page=<?= ($current_page + 1); ?>"><?php endif; ?>Вперед</a></li>
         <?php endif; ?>
     </ul>
 <?php endif; ?>
